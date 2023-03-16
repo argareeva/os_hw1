6db85f3fefdee5f0bdf9d6a6ba01d8af7ddfded4
argareeva <116667251+argareeva@users.noreply.github.com>
2023-03-16 17:25:18 +0500

    Create 7_баллов.c
    Программа создает два именованных канала, используя mkfifo, один для связи между первым и вторым дочерними процессами (pipe1), а другой — для связи между вторым дочерним процессом и родительским процессом (pipe2).
    Первый дочерний процесс читает из входного файла и записывает в канал 1. 
    Второй дочерний процесс читает из канала 1, обрабатывает данные и записывает результат в канал 2. 
    Наконец, родительский процесс читает из канала 2 и записывает в выходной файл.
    Таким образом, первый дочерний процесс отвечает за ввод и вывод, а второй дочерний процесс обрабатывает данные.
    
    После завершения выполнения программы используем unlink для удаления именованных каналов.

---
+++ 7_баллов.c
@@ -0,0 +1,150 @@
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFSIZE 5000
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s input_file output_file\n", argv[0]);
+        return 1;
+    }
+    
+    //Аргументы командной строки
+    char *input_file = argv[1];
+    char *output_file = argv[2];
+
+    int fd1, fd2;
+    pid_t pid1, pid2;
+
+    //Создание 1 именованного канала
+    if (mkfifo("pipe1", 0666) == -1) {
+        perror("mkfifo");
+        return 1;
+    }
+
+    //Создание 2 именованного канала
+    if (mkfifo("pipe2", 0666) == -1) {
+        perror("mkfifo");
+        return 1;
+    }
+
+    //Разветвляем первый дочерний процесс для чтения из входного файла и записи в канал
+    if ((pid1 = fork()) == -1) {
+        perror("fork");
+        return 1;
+    } else if (pid1 == 0) {
+        char buf[BUFSIZE];
+        int n;
+        //Открываем 1 дочерний процесс для записи
+        if ((fd1 = open("pipe1", O_WRONLY)) == -1) {
+            perror("open");
+            return 1;
+        }
+
+        //Открываем входной файл для чтения
+        if ((fd2 = open(input_file, O_RDONLY)) == -1) {
+            perror("open");
+            return 1;
+        }
+        
+        //Читаем из входного файла и записываем в 1 канал
+        while ((n = read(fd2, buf, BUFSIZE)) > 0) {
+            if (write(fd1, buf, n) == -1) {
+                perror("write");
+                return 1;
+            }
+        }
+
+        //Закрываем файловые дескрипторы
+        close(fd1);
+        close(fd2);
+
+        return 0;
+    }
+
+    //Разветвляем второй дочерний процесс для чтения из 1 канала
+    //Результат обработки передаем обратно 1 процессу через именованный канал
+    if ((pid2 = fork()) == -1) {
+        perror("fork");
+        return 1;
+    } else if (pid2 == 0) {
+        //Открываем 1 канал для чтения
+        if ((fd1 = open("pipe1", O_RDONLY)) == -1) {
+            perror("open");
+            return 1;
+        }
+
+        //Открываем 2 канал для записи
+        if ((fd2 = open("pipe2", O_WRONLY)) == -1) {
+            perror("open");
+            return 1;
+        }
+        //Читаем из 1 канала и записываем результат во 2 канал
+        char buf;
+        char num_buf[BUFSIZE];
+        int num_pos = 0;
+        //цикл обработки первоначальной строки и записи новой строки во 2 канал
+        while (read(fd1, &buf, 1) > 0) {
+            if (isdigit(buf)) {
+                num_buf[num_pos++] = buf;
+            } else if (num_pos > 0) {
+                num_buf[num_pos++] = '+';
+                write(fd2, num_buf, num_pos);
+                num_pos = 0;
+            }
+        }
+
+        if (num_pos > 0) {
+            num_buf[num_pos++] = '+';
+            write(fd2, num_buf, num_pos);
+        }
+        
+        //Закрываем файловые дескрипторы
+        close(fd1);
+        close(fd2);
+
+        return 0;
+    }
+    //Возвращаемся к родительскому процессу
+    char buf[BUFSIZE];
+    int n;
+
+    //Открываем 2 канал для чтения
+    if ((fd1 = open("pipe2", O_RDONLY)) == -1) {
+            perror("open");
+            return 1;
+        }
+
+    //Открываем выходной файл для записи
+    if ((fd2 = open(output_file, O_WRONLY | O_CREAT, 0666)) == -1) {
+        perror("open");
+        return 1;
+    }
+
+    //Читаем из 2 канала и записываем в выходной файл
+        while ((n = read(fd1, buf, BUFSIZE)) > 0) {
+            if (write(fd2, buf, n) == -1) {
+                perror("write");
+                return 1;
+            }
+        }
+
+    close(fd1);
+    close(fd2);
+
+    //Завершаем дочерние процессы
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+    wait(NULL);
+    unlink("pipe1");
+    unlink("pipe2");
+
+    return 0;
+}
